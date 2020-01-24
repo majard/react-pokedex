0351bb5839fd427722c981f67ec5416a0acf1bff
Mah <mahjard@protonmail.com>
2020-01-24 15:43:46 -0300

    Added pagination and some css to the pokedex app

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -11,10 +11,15 @@ import { Link } from 'react-router-dom'
 class PokemonList extends React.Component{
   constructor(props){
     super(props);
+    this.updateURL = this.updateURL.bind(this);
+    this.getPokemonList = this.getPokemonList.bind(this);
     this.state = {
       species : [],
       fetched : false,
       loading : false,
+      next: null,
+      previous: null,
+      offset: 1,
     };
   }
 
@@ -23,31 +28,80 @@ class PokemonList extends React.Component{
     this.setState({
       loading : true
     });
-    fetch('http://pokeapi.co/api/v2/pokemon?limit=151').then(res=>res.json())
+    this.getPokemonList('http://pokeapi.co/api/v2/pokemon?limit=151');
+  }
+
+  getPokemonList(url){
+    fetch(url).then(res=>res.json())
     .then(response=>{
+      console.log("response.results", response);
       this.setState({
         species : response.results,
         loading : true,
-        fetched : true
+        fetched : true,
+        next : response.next,
+        previous: response.previous,
       });
     });
   }
 
+  // updates the state of the url, and then call getPokemonList to build
+  // the new list, as the second argument to setState
+  updateURL(navigation){
+    console.log("url", navigation);
+    if (navigation === "previous"){
+      let url = this.state.previous;
+      this.getPokemonList(url);
+      this.setState({fetched: false,
+                     offset: this.state.offset - 151,
+                   });
+    } else if (navigation === "next") {
+      let url = this.state.next;
+      this.getPokemonList(url);
+      this.setState({fetched: false,
+                     offset: this.state.offset + 151,
+                   });
+    }
+  }
+
   render(){
-    const {fetched, loading, species} = this.state;
+    const {fetched, loading, species, offset} = this.state;
     let content ;
+    let nextButton, previousButton;
+    console.log("species: ",species);
+
+    if (this.state.previous){
+      previousButton = <button
+                      onClick={() => this.updateURL("previous")}>
+                        Previous
+                      </button>;
+    }
+    if (this.state.next) {
+      nextButton = <button
+                    onClick={() => this.updateURL("next")}>
+                      Next
+                   </button>;
+    }
     if(fetched){
       content = (
-        <div className="pokemon-list">
-          {species.map(
-            (pokemon,index)=>
-              <Link to={"/pokemon/"+(index+1)}
-              key={pokemon.name}
-              onClick={() => this.props.handleClick(index + 1, pokemon)}>
-                <Pokemon id={index+1} pokemon={pokemon}/>
-              </Link>
-          )}
-        </div>);
+
+        <div className="pokemon-list-container">
+          <div className="nav-button-container"> 
+            {previousButton} {nextButton}
+          </div>
+
+          <div className="pokemon-list">
+            {species.map(
+              (pokemon,index)=>
+                <Link to={"/pokemon/"+(index+offset)}
+                key={pokemon.name}
+                onClick={() => this.props.handleClick(index + offset, pokemon)}>
+                  <Pokemon id={index+offset} pokemon={pokemon}/>
+                </Link>
+            )}
+          </div>
+        </div>
+      );
     }else if(loading && !fetched){
         content = <p> Loading ...</p>;
     }
